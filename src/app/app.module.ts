8771e07ca39c9e2caa5bea68226322283dfb6f4f
samxd29 <carvalholuzs@gmail.com>
2023-01-27 18:10:27 -0300

    Definindo e extraindo parâmetros de url (query)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,12 +12,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule, routing } from './app-routing';
 import { AppComponent } from './app.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { CursosService } from './cursos/cursos/cursos.service';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
-
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso
     routing,
     MatToolbarModule,
     MatIconModule,
-
+    MatPaginatorModule,
+    MatButtonModule
   ],
   providers: [CursosService],
   bootstrap: [AppComponent]
